de56a3288b7afa817945b03265c6831dbfaf2017
Tycho-Svoboda <tychosvoboda@gmail.com>
2019-06-23 15:22:12 -0400

    Add files via upload

---
+++ hailey day 1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+""""First, explain how computer science is important, and how it can be used to make video games, art, cars, computers, 
+and basically everything!
+
+Next, explain how there are multiple computer programming languages, and we will be working in python
+
+Demonstrate a print statement, and then have student write their own.
+"""
+
+
+# In[ ]:
+
+
+print('my name is Tycho')
+
+
+# In[7]:
+
+
+print ('my name is hailey')
+
+
+# In[ ]:
+
+
+"""Next, go into data types, and explain how Python has 3 main types (string, boolean, and integers).  Explain each 
+type, and give an example of each.  Then have the student write a couple examples of each type
+
+
+# In[8]:
+
+
+#string
+name = 'hailey'
+#boolean (true or false)
+wearing_grey_shirt = True
+#integer
+integer = 9
+
+
+# In[9]:
+
+
+x = 10
+i_have_a_pink_shirt = True
+pet = 'bird'
+i_am_working_on_coding=True
+
+
+# In[ ]:
+
+
+"""This next part is trickier.  You have to introduce input statement and what it does, and show an example of a 
+calculator.  Walk through each step, and have student create their own calculator
+
+
+# In[26]:
+
+
+
+x=int(input('Hi there! Please input a number from 1-100 '))
+y = int(input('Now, please input a number from 1-100 again '))
+z = str(x+y)
+print('if you add the two numbers together, you get ' + z)
+
+
+# 
+
+# In[ ]:
+
+
+x=int(input('Hi there! Please input a number from 1-100'))
